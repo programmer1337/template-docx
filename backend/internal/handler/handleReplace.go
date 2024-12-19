4b719baf22679d3c6a1f304b86b7ae3b81186f66
Dmitrii <dmitii.miroshnikov@gmail.com>
2024-12-19 18:07:55 +0300

    test

--- backend/internal/handler/handleReplace.go
+++ backend/internal/handler/handleReplace.go
@@ -56,6 +56,7 @@ func Replace(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	log.Print("read body")
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
 		log.Printf("Error reading request body: %v", err)
