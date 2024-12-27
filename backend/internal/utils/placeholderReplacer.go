bbadb55bae0c56cf1ecc7a91f7e7fbe6d580e989
Dmitrii <dmitii.miroshnikov@gmail.com>
2024-12-27 17:05:03 +0300

    add error handle

--- backend/internal/utils/placeholderReplacer.go
+++ backend/internal/utils/placeholderReplacer.go
@@ -1,22 +1,28 @@
 package utils
 
-import "github.com/lukasjarosch/go-docx"
+import (
+	"fmt"
 
-func PlaceholderReplacer(pathToTemplate string, pathToSave string, replaceMap docx.PlaceholderMap) {
+	"github.com/lukasjarosch/go-docx"
+)
+
+func PlaceholderReplacer(pathToTemplate string, pathToSave string, replaceMap docx.PlaceholderMap) error {
 	// read and parse the template docx
 	// docx.ChangeOpenCloseDelimiter('$', '$')
 	doc, err := docx.Open(pathToTemplate)
 	if err != nil {
-		panic(err)
+		return fmt.Errorf("Can't open file")
 	}
 
 	err = doc.ReplaceAll(replaceMap)
 	if err != nil {
-		panic(err)
+		return fmt.Errorf("Can't replace. [%v]", err)
 	}
 
 	err = doc.WriteToFile(pathToSave)
 	if err != nil {
-		panic(err)
+		return fmt.Errorf("Can't write. [%v]", err)
 	}
+
+	return nil
 }
